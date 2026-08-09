75c02af997ed8e10bab6d8202d33278dc9f00996
marbotte <bottinmarius@gmail.com>
2026-08-08 21:26:13 -0500

    small fix, the comparison of the file in the zip and the destfile was failing when no distfile

--- utils/download.py
+++ utils/download.py
@@ -84,7 +84,7 @@ def crc32(fileName):
         return format(hash & 0xFFFFFFFF, '08x')
 
 def extract_gbifZip(zipFile, nameInZip, destFile, checkHash=True):
-  if checkHash:
+  if checkHash and os.path.exist(destFile):
     if crc32_file_in_zip(zipFile,nameInZip) == crc32(destFile):
       print("The current file has a corresponding hash to the one in the zip archive, skipping extraction")
       return destFile
